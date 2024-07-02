e672c0f399bba63d51b04c747ea8a56968334dc6
Yilchik <yulyayulia2@gmail.com>
2024-07-02 19:15:32 +0200

    Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import AppBar from "../AppBar/AppBar";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <>
       <AppBar />
       <main>
         <Outlet />
+        {children}
       </main>
     </>
   );
